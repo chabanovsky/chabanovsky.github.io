313089464867506adbcbb6544757cb7e441bee2f
Nicolas Chabanovsky <nicolas@chabanovsky.com>
2015-09-07 11:38:32 -0400

    Added google analytics

--- js/stackoverflow_widget_client.js
+++ js/stackoverflow_widget_client.js
@@ -1,6 +1,6 @@
 
 $(function() {
-    init(['1', '2', '3', '4', '5', '6'], "CYU2EOKOmtp434pvPMA2ww((");
+    init(['1', '6'], "CYU2EOKOmtp434pvPMA2ww((");
     root_element = $("#questions_root");
     CreateQuestionsFeed(root_element);
 });
